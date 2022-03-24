3159ab9e426a0ce941424d863275537a8246e5f2
DroidXrX <droidxrx@gmail.com>
2022-03-24 12:43:17 +0700

    add export color

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,12 +3,14 @@ import memory from './memory';
 import misc from './misc';
 import overlay from './overlay';
 import vector from './vector';
+import color from './color';
 
 export * from './overlay';
 export * from './draw';
 export * from './misc';
 export * from './vector';
 export * from './memory';
+export * from './color';
 
 export default {
 	...overlay,
@@ -16,4 +18,5 @@ export default {
 	...misc,
 	...vector,
 	...memory,
+	color,
 };
