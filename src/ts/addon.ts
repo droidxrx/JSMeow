ca1025733d22645f3fec51b24b8e47f5ac2eb177
DroidXrX <droidxrx@gmail.com>
2022-03-28 01:04:21 +0700

    cleaning

--- src/ts/addon.ts
+++ src/ts/addon.ts
@@ -73,162 +73,34 @@ interface Addons {
 		overlayInit: (target: string, exitkey: number, borderOffset: number) => Overlay;
 	};
 	draws: {
-		drawAlphaBox: (
-			x: number,
-			y: number,
-			width: number,
-			height: number,
-			color: RGB,
-			outlineColor: RGB,
-			alpha: number
-		) => void;
-		drawAlphaBoxV: (
-			pos: Vector2,
-			width: number,
-			height: number,
-			color: RGB,
-			outlineColor: RGB,
-			alpha: number
-		) => void;
-		drawBox: (
-			x: number,
-			y: number,
-			width: number,
-			height: number,
-			lineWidth: number,
-			color: RGB
-		) => void;
-		drawBoxV: (
-			pos: Vector2,
-			width: number,
-			height: number,
-			lineWidth: number,
-			color: RGB
-		) => void;
-		drawCircle: (
-			x: number,
-			y: number,
-			radius: number,
-			color: RGB,
-			filled: boolean
-		) => void;
+		drawAlphaBox: (x: number, y: number, width: number, height: number, color: RGB, outlineColor: RGB, alpha: number) => void;
+		drawAlphaBoxV: (pos: Vector2, width: number, height: number, color: RGB, outlineColor: RGB, alpha: number) => void;
+		drawBox: (x: number, y: number, width: number, height: number, lineWidth: number, color: RGB) => void;
+		drawBoxV: (pos: Vector2, width: number, height: number, lineWidth: number, color: RGB) => void;
+		drawCircle: (x: number, y: number, radius: number, color: RGB, filled: boolean) => void;
 		drawCircleV: (pos: Vector2, radius: number, color: RGB, filled: boolean) => void;
-		drawCornerBox: (
-			x: number,
-			y: number,
-			width: number,
-			height: number,
-			color: RGB,
-			outlineColor: RGB,
-			lineWidth: number
-		) => void;
-		drawCornerBoxV: (
-			pos: Vector2,
-			width: number,
-			height: number,
-			color: RGB,
-			outlineColor: RGB,
-			lineWidth: number
-		) => void;
-		drawCustomShape: (
-			points: Vector2[],
-			color: RGB,
-			filled: boolean,
-			alpha: number
-		) => void;
-		drawDashedLine: (
-			x1: number,
-			y1: number,
-			x2: number,
-			y2: number,
-			lineWidth: number,
-			factor: number,
-			pattern: number,
-			color: RGB,
-			alpha: number
-		) => void;
-		drawDashedLineV: (
-			pos1: Vector2,
-			pos2: Vector2,
-			lineWidth: number,
-			factor: number,
-			pattern: number,
-			color: RGB,
-			alpha: number
-		) => void;
-		drawLine: (
-			x1: number,
-			y1: number,
-			x2: number,
-			y2: number,
-			lineWidth: number,
-			color: RGB
-		) => void;
+		drawCornerBox: (x: number, y: number, width: number, height: number, color: RGB, outlineColor: RGB, lineWidth: number) => void;
+		drawCornerBoxV: (pos: Vector2, width: number, height: number, color: RGB, outlineColor: RGB, lineWidth: number) => void;
+		drawCustomShape: (points: Vector2[], color: RGB, filled: boolean, alpha: number) => void;
+		drawDashedLine: (x1: number, y1: number, x2: number, y2: number, lineWidth: number, factor: number, pattern: number, color: RGB, alpha: number) => void;
+		drawDashedLineV: (pos1: Vector2, pos2: Vector2, lineWidth: number, factor: number, pattern: number, color: RGB, alpha: number) => void;
+		drawLine: (x1: number, y1: number, x2: number, y2: number, lineWidth: number, color: RGB) => void;
 		drawLineV: (pos1: Vector2, pos2: Vector2, lineWidth: number, color: RGB) => void;
 		drawPixel: (x: number, y: number, color: RGB) => void;
 		drawPixelV: (pos: Vector2, color: RGB) => void;
-		drawPoly: (
-			x: number,
-			y: number,
-			radius: number,
-			rotation: number,
-			sides: number,
-			color: RGB
-		) => void;
-		drawPolyV: (
-			pos: Vector2,
-			radius: number,
-			rotation: number,
-			sides: number,
-			color: RGB
-		) => void;
-		drawRadCircle: (
-			x: number,
-			y: number,
-			radius: number,
-			startValue: number,
-			endValue: number,
-			color: RGB
-		) => void;
-		drawRadCircleV: (
-			pos: Vector2,
-			radius: number,
-			startValue: number,
-			endValue: number,
-			color: RGB
-		) => void;
-		drawTextLines: (
-			font: Font,
-			x: number,
-			y: number,
-			lines: string[],
-			color: RGB,
-			offset: number
-		) => void;
-		drawValueBar: (
-			x1: number,
-			y1: number,
-			x2: number,
-			y2: number,
-			width: number,
-			maxValue: number,
-			value: number,
-			vertical: boolean
-		) => void;
-		drawValueBarV: (
-			pos1: Vector2,
-			pos2: Vector2,
-			width: number,
-			maxValue: number,
-			value: number,
-			vertical: boolean
-		) => void;
+		drawPoly: (x: number, y: number, radius: number, rotation: number, sides: number, color: RGB) => void;
+		drawPolyV: (pos: Vector2, radius: number, rotation: number, sides: number, color: RGB) => void;
+		drawRadCircle: (x: number, y: number, radius: number, startValue: number, endValue: number, color: RGB) => void;
+		drawRadCircleV: (pos: Vector2, radius: number, startValue: number, endValue: number, color: RGB) => void;
+		drawTextLines: (font: Font, x: number, y: number, lines: string[], color: RGB, offset: number) => void;
+		drawValueBar: (x1: number, y1: number, x2: number, y2: number, width: number, maxValue: number, value: number, vertical: boolean) => void;
+		drawValueBarV: (pos1: Vector2, pos2: Vector2, width: number, maxValue: number, value: number, vertical: boolean) => void;
 		drawText: (font: Font, x: number, y: number, text: string, color: RGB) => void;
 	};
 	misc: {
 		isKeyPressed: (vkey: number) => boolean;
 		mouseClick: (leftOrRight: boolean) => void;
-		mouseMove: (overlay: Overlay, x: number, y: number) => void;
+		mouseMove: (x: number, y: number) => number;
 		pressKey: (vkey: number) => void;
 		setForeground: (winTitle: string) => boolean;
 	};
@@ -238,39 +110,13 @@ interface Addons {
 		getProcesses: () => ProcessObject[];
 		getModules: (processId: number) => ModuleObject[];
 		findModule: (moduleName: string, processId: number) => ModuleObject;
-		readMemory: <T extends dataType>(
-			handle: number,
-			address: number,
-			dataType: T
-		) => ReturnType<T>;
+		readMemory: <T extends dataType>(handle: number, address: number, dataType: T) => ReturnType<T>;
 		readBuffer: (handle: number, address: number, size: number) => Buffer;
-		writeMemory: (
-			handle: number,
-			address: number,
-			value: writeValue,
-			dataType: dataType
-		) => void;
+		writeMemory: (handle: number, address: number, value: writeValue, dataType: dataType) => void;
 		writeBuffer: (handle: number, address: number, buffer: Buffer) => void;
-		findPattern: (
-			handle: number,
-			pattern: string,
-			flags: number,
-			patternOffset: number
-		) => number;
-		findPatternByModule: (
-			handle: number,
-			moduleName: string,
-			pattern: string,
-			flags: number,
-			patternOffset: number
-		) => number;
-		findPatternByAddress: (
-			handle: number,
-			baseAddress: number,
-			pattern: string,
-			flags: number,
-			patternOffset: number
-		) => number;
+		findPattern: (handle: number, pattern: string, flags: number, patternOffset: number) => number;
+		findPatternByModule: (handle: number, moduleName: string, pattern: string, flags: number, patternOffset: number) => number;
+		findPatternByAddress: (handle: number, baseAddress: number, pattern: string, flags: number, patternOffset: number) => number;
 		injectDll: (handle: number, dllPath: string) => void;
 		unloadDll: (handle: number, moduleNameOrModuleBaseAddress: string | number) => void;
 	};
